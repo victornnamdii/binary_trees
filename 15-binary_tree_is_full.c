0530cfbe701cc01f59d6d7ae2f41ebc0661ec107
“victornnamdii” <victorilodiuba@gmail.com>
2022-09-20 23:02:54 +0100

    Edit(1): 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -22,7 +22,7 @@ size_t binary_tree_is_full(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (binary_tree_is_leaf(tree) && tree->parent)
+	if (binary_tree_is_leaf(tree))
 		return (1);
 	left = binary_tree_is_full(tree->left);
 	right = binary_tree_is_full(tree->right);
